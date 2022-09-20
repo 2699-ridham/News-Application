400a84f408ee68d7ef74b098c3b430b161d0e477
2699-ridham <ridhamgarg786@gmail.com>
2022-09-20 15:04:35 +0530

    Completed class based component

--- src/components/News.js
+++ src/components/News.js
@@ -1,105 +1,105 @@
 import React, { Component } from 'react'
 import Newsitems from './Newsitems';
 import { Spinner } from './spinner';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export default class News extends Component {
     //static means remain same through out the code where they will be used;
     //defaultProps -> asigning value to some variables, if case use didn't any props they will get fire;
-    static defaultProps={
+    static defaultProps = {
         country: 'in',
         pageSize: 8,
         category: 'general'
     }
 
     //propTypes -> defines the types of the default props i.e number, String, boolean etc.
-    static propTypes={
+    static propTypes = {
         country: PropTypes.string,
         pageSize: PropTypes.number,
         category: PropTypes.string
     }
-    constructor(){
+
+    capitalizeFunction = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    //if we want to use the props inside the constructor then we have to mention props
+    constructor(props) {
         //whenever want to access the constructor then super key is mention to be mandatory;
-        super();
+        super(props);
         //basically we can declare our state through the constructor
-        this.state={
+        this.state = {
             articles: [],
-            loading: false,
-            page:1
+            loading: true,
+            page: 1,
+            totalResults: 0
         }
+
+        document.title = `${this.capitalizeFunction(this.props.category)} News-DailyHunt`;
     }
 
-    //it is a life cycle method -> and run after the rander function -> helps to fetch data through api
-    async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1efe9f0d13a14c28810decf22aa9bf42&page=1&pageSize=${this.props.pageSize}`;
-        //fetch -> promise we have to apply await;
-        this.setState(
-            {loading: true}
-        )
+    async updateNews() {
+        this.props.setProgress(10);
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true })
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parseData = await data.json();
-        // console.log(parseData);
+        this.props.setProgress(70);
         this.setState({
-            articles : parseData.articles,
+            articles: parseData.articles,
             totalResults: parseData.totalResults,
-            loading : false
-        });
-    }
- 
-    handlePrev= async ()=>{
-        console.log("Previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1efe9f0d13a14c28810decf22aa9bf42&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading: true})
-        let data = await fetch(url);
-        let parseData = await data.json();
-        this.setState({
-            page : this.state.page - 1,
-            articles : parseData.articles,
             loading: false
         })
+        this.props.setProgress(100);
     }
-    handleNext= async()=>{
-        console.log("Next");
-        if(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)){
+    //it is a life cycle method -> and run after the rander function -> helps to fetch data through api
+    async componentDidMount() {
+        this.updateNews();
+    }
 
-        }else{
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=1efe9f0d13a14c28810decf22aa9bf42&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({loading: true})
-            let data = await fetch(url);
-            let parseData = await data.json();
-            this.setState({
-                page : this.state.page + 1,
-                articles : parseData.articles,
-                loading: false
-            })
-        }
-    }
-  render() {
-    return (
-      <div>
-            <div className='container my-4'>
-                <h2 className="text-center">News DailyHunt - Top Headlines</h2>
-                {this.state.loading && <Spinner/>}
-                <div className='row'>
-                    {/*row -> taking 3 items in a row*/}
-                    {this.state.articles.map((element)=>{
-                        //key: -> important to give to identify each element uniquely
-                        return <div className='col-md-4' key={element.url}>
-                        {/* col-md-4 -> "md" -> medium device takes for 4 column  from grid i.e bootstrap contains grid of 12*/}
-                        {/*col-md-4 makes align 3 boxes in one line with space of 4 grid*/}
-                            <Newsitems title={element.title ? element.title.slice(0,45) : ""} 
-                            description={element.description ? element.description.slice(0,88) : ""} 
-                            imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}
-                            source={element.source.name}/>
+    fetchMoreData = async () => {
+        // a  async api call like which sends
+        this.setState({ page: this.state.page + 1 });
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parseData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parseData.articles),
+            totalResults: parseData.totalResults,
+        })
+    };
+
+    render() {
+        return (
+            <>
+                <h2 className="text-center" style={{margin: '35px 0px', marginTop: '90px'}}>News DailyHunt - Top {this.capitalizeFunction(this.props.category)} Headlines</h2>
+                {this.state.loading && <Spinner />}
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                >
+                    <div className='container'>
+                        <div className='row'>
+                            {/*row -> taking 3 items in a row*/}
+                            {this.state.articles.map((element) => {
+                                //key: -> important to give to identify each element uniquely
+                                return <div className='col-md-4' key={element.content}>
+                                    {/* col-md-4 -> "md" -> medium device takes for 4 column  from grid i.e bootstrap contains grid of 12*/}
+                                    {/*col-md-4 makes align 3 boxes in one line with space of 4 grid*/}
+                                    <Newsitems title={element.title ? element.title : ""}
+                                        description={element.description ? element.description.slice(0, 88) : ""}
+                                        imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}
+                                        source={element.source.name} />
+                                </div>
+                            })}
                         </div>
-                    })}
-                </div>
-            </div>
-            <div className='container d-flex justify-content-between mb-3'>
-                <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrev}> &larr; Previous</button>
-                <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
-            </div>
-      </div>
-    )
-  }
+                    </div>
+                </InfiniteScroll>
+            </>
+        )
+    }
 }
--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -2,24 +2,26 @@ import React, { Component } from 'react'
 
 export class Newsitems extends Component {
   render() {
-      let {title,description,imageUrl,newsUrl,author,date,source} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
-        <div>
-            <div className="card my-4">
-              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{left: '90%' ,zIndex: '1'}}>{source}</span>
-              {/*if in case imageUrl is null -> attach below link */}
-             <img src={imageUrl == null ? "https://th.bing.com/th/id/OIP.np1BGMCMmFhrQ9WCP1SeTgHaE7?pid=ImgDet&rs=1" : imageUrl} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{title}...</h5>
-                    <p className="card-text">{description}...</p>
-                    {/*btn-sm is a bootstrap class which make the button of smaller length */}
-                    <p className="card-text"><small className="text-muted">By {author ==null ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
-                                                                      {/*let a = "any date time format ->"new Date(a) -> obj created and then we convert it
+      <div className='my-4'>
+        <div className="card">
+          <div style={{display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0'}}>
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+          {/*if in case imageUrl is null -> attach below link */}
+          <img src={imageUrl == null ? "https://th.bing.com/th/id/OIP.np1BGMCMmFhrQ9WCP1SeTgHaE7?pid=ImgDet&rs=1" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            {/*btn-sm is a bootstrap class which make the button of smaller length */}
+            <p className="card-text"><small className="text-muted">By {author == null ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+            {/*let a = "any date time format ->"new Date(a) -> obj created and then we convert it
                                                                                                      into any of the format of date and time*/}
-                    <a  rel="noopener noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-                </div>
-             </div>
+            <a rel="noopener noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+          </div>
         </div>
+      </div>
     )
   }
 }
